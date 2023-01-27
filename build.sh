e2e18813b1e25661977042b2c0b8b63b1312d476
Ashwin Godbole <dev.godboleashwin@gmail.com>
2023-01-27 15:32:59 +0530

    site rebuild

--- build.sh
+++
@@ -1,5 +0,0 @@
-html='index.html'
-html_t='tmpl.html'
-css='style.css'
-src='index.sfl'
-sitefl -nts $html_t $css $src $html && echo "LOG : compiled file to html."
---
+++ gen.sh
@@ -0,0 +1,2 @@
+wrang index.wr index.html -css stylesheet.css -title Home
+wrang resume.wr resume.html -css stylesheet.css -title Resume
