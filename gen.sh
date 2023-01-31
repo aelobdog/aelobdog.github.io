cdc7234cdc1a9978424ee8f728bb4e4be20fc452
Ashwin Godbole <dev.godboleashwin@gmail.com>
2023-01-31 21:52:39 +0530

    added projects page

--- gen.sh
+++ gen.sh
@@ -1,2 +1,3 @@
 wrang index.wr index.html -css stylesheet.css -title Home
 wrang resume.wr resume.html -css stylesheet.css -title Resume
+wrang projects.wr projects.html -css stylesheet.css -title Projects
